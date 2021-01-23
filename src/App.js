b1151c165c51d21fb542426039afd99f4440b37f
pzjiang <pzjiang01@hotmail.com>
2021-01-23 17:16:26 -0500

    start api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useCallback } from "react"
 
 function App() {
+  const nameEl = React.useRef(null);
+  const passEl = React.useRef(null);
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+
+  const onClick = useCallback(() => {
+
+    alert("username: " + userName + "\npassword: " +
+      passWord);
+  });
+
+  const onPassChange = useCallback(() => {
+    setPassWord(passEl.current.value);
+  })
+
+  const onNameChange = useCallback(() => {
+    setUserName(nameEl.current.value);
+  });
+
+  const forgotPass = useCallback(() => {
+    alert("Too bad lmao be better");
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login in to begin
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={onClick}>
+          <label>
+            UserName:
+            <input type="text" placeholder="username" ref={nameEl} onChange={onNameChange} />
+          </label>
+          <label>
+            Password:
+            <input type="password" placeholder="password" ref={passEl} onChange={onPassChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <button onClick={forgotPass}>
+          Forgot Your Password?
+        </button>
       </header>
     </div>
   );
